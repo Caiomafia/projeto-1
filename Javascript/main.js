f4829a9f49217f5d540ff5a976ae95a89c089911
Caio Mafra <gamersb200@gmail.com>
2021-10-23 09:29:46 -0300

    atualizaãção

---
+++ Javascript/main.js
@@ -0,0 +1,33 @@
+function clique() { 
+    document.getElementById("agradecimento") .innerHTML="obrigado por clicar ";
+    console.log(document.getElementById("agradecimento"));
+}
+
+
+/*var d = new Date;
+alert(d.getMonth()+1);
+alert(d.getTime()+1);
+alert(d.getDay()+1);
+alert(d.getMinutes()+1);*/
+
+/*var count;
+for( count=0 ; count >=5 ; count++);{
+    alert(count+ 1);
+};*/
+
+
+
+/*var count=3;
+while (count>=5){
+    console.log (count);
+    alert(count);
+}*/
+  
+
+/* var idade=prompt ("qual sua idade");
+
+if(   idade >= 18) 
+{ alert("maior de idade");
+}else{
+    alert("menor de idade")
+};*/
